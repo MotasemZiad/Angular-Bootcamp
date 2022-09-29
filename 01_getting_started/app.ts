1ab05b2c6bec81fa1769d70fe2b9d59556ea3eb5
MotasemZiad <moatasem.abunema@gmail.com>
2022-09-29 15:30:14 +0300

    some updates

--- 01_getting_started/app.ts
+++ 01_getting_started/app.ts
@@ -1,1 +1,15 @@
 console.log("Hello, TypeScript!")
+
+const user = {
+    "username": "motasemabunema",
+    "display_name": "Motasem Abu Nema",
+    "email": "moatasem.abunema@gmail.com",
+    "contact": {
+        "phone": "082811040",
+        "mobile": "970599838964",
+    },
+    "graduated": false,
+    "alive": true,
+}
+
+console.log(user)
