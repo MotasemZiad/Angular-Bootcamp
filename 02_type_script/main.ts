a280e755e9863aae123556259deb77fee2374618
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-05 15:43:34 +0300

    start with types

--- 02_type_script/main.ts
+++ 02_type_script/main.ts
@@ -1,21 +1,24 @@
-function log(message){
-    console.log(message);
-}
-var message = 'Hello World!';
-log(message);
+// function log(message){
+//     console.log(message);
+// }
+// var message = 'Hello World!';
+// log(message);
 
-var number = 10; // Variable 
-let count = 1; // Final
+// var number = 10; // Variable 
+// let count = 1; // Final
 
-console.log(number);
-console.log(count);
+// console.log(number);
+// console.log(count);
 
-function doSomething(){
-    for(let i = 0; i < 5; i++){
-        console.log(i);
-    }
-    console.log("Finally: "+ i);
+// function doSomething(){
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log("Finally: "+ i);
     
-}
+// }
 
-doSomething(); 
+// doSomething(); 
+
+// Types
+
