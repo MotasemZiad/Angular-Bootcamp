18c517ab5e1ce232b7cf2e7853b784e31d33b120
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-05 15:36:51 +0300

    updates regarding ts

--- 02_type_script/main.js
+++ 02_type_script/main.js
@@ -1,21 +1,16 @@
-function log(message){
+function log(message) {
     console.log(message);
 }
 var message = 'Hello World!';
 log(message);
-
 var number = 10; // Variable 
-let count = 1; // Final
-
+var count = 1; // Final
 console.log(number);
 console.log(count);
-
-function doSomething(){
-    for(let i = 0; i < 5; i++){
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
         console.log(i);
     }
-    console.log("Finally: "+ i);
-    
+    console.log("Finally: " + i);
 }
-
-doSomething(); 
+doSomething();
