f1def6a8a040751ab34a19833d7793b6bbb01660
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-06 15:30:47 +0300

    classes

--- 02_type_script/main.ts
+++ 02_type_script/main.ts
@@ -20,52 +20,68 @@
 
 // doSomething(); 
 
-// Types
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[];
-let f: any[] = [1, true, 'Ahmed', 49.23, ["Programming", "Database"], {"username": "Ahmed Sami", "email": "ahmed.sami@gmail.com"}];
+// // Types
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[];
+// let f: any[] = [1, true, 'Ahmed', 49.23, ["Programming", "Database"], {"username": "Ahmed Sami", "email": "ahmed.sami@gmail.com"}];
 
-console.log(f)
+// console.log(f)
 
 
-enum Color {Red, Green, Blue};
+// enum Color {Red, Green, Blue};
 
-console.log(Color.Red, Color.Green, Color.Blue);
+// console.log(Color.Red, Color.Green, Color.Blue);
 
-let message = 'abc';
-let characters = message.split('');
-console.log(characters);
+// let message = 'abc';
+// let characters = message.split('');
+// console.log(characters);
 
-// Type Assertion
-let msg;
-msg = 'abcd';
-let endsWith = (<string>msg).endsWith('d');
-let alternativeWay = (msg as string).endsWith('c');
-console.log(endsWith, alternativeWay);
+// // Type Assertion
+// let msg;
+// msg = 'abcd';
+// let endsWith = (<string>msg).endsWith('d');
+// let alternativeWay = (msg as string).endsWith('c');
+// console.log(endsWith, alternativeWay);
 
-// Arrow Function
-let log = function(message: any){
-    console.log(message);
-}
+// // Arrow Function
+// let log = function(message: any){
+//     console.log(message);
+// }
 
-let doLog = (message: any)=> console.log(message);
+// let doLog = (message: any)=> console.log(message);
 
-// Interfaces
-let drawPoint = (point: Point) => {
-    // Your code logic
-    console.log(point);
-}
+// // Interfaces
+// let drawPoint = (point: Point) => {
+//     // Your code logic
+//     console.log(point);
+// }
 
-drawPoint({
-    x: 2, 
-    y: 3
-});
+// drawPoint({
+//     x: 2, 
+//     y: 3
+// });
 
-interface Point{
+// interface Point{
+//     x: number
+//     y: number
+//     draw: () => void
+//     getDistance: () => number
+// }
+
+// Classes
+class Point {
     x: number
     y: number
+
+    draw(){
+        //..
+    }
+
+    getDistance(another: Point){
+        // .. 
+    }
 }
 
