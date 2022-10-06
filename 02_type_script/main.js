f81c45afa4773361f56598141175acad508bd5e3
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-06 14:21:11 +0300

    more on types

--- 02_type_script/main.js
+++ 02_type_script/main.js
@@ -3,37 +3,18 @@
 // }
 // var message = 'Hello World!';
 // log(message);
-
 // var number = 10; // Variable 
 // let count = 1; // Final
-
 // console.log(number);
 // console.log(count);
-
 // function doSomething(){
 //     for(let i = 0; i < 5; i++){
 //         console.log(i);
 //     }
 //     console.log("Finally: "+ i);
-    
 // }
-
 // doSomething(); 
-
 // Types
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[];
-let f: any[] = [1, true, 'Ahmed', 49.23, ["Programming", "Database"], {"username": "Ahmed Sami", "email": "ahmed.sami@gmail.com"}];
-
-console.log(f)
-
-
-enum Color {Red, Green, Blue};
-
-console.log(Color.Red, Color.Green, Color.Blue);
-
-
-
+var count = 5;
+count = 'a';
+console.log(count);
