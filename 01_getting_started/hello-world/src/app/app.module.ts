4e3444077695463d3dc0f1df828e8a82994584bf
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-10 13:37:29 +0300

    getting started with components

--- 01_getting_started/hello-world/src/app/app.module.ts
+++ 01_getting_started/hello-world/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
