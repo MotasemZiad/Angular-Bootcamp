ac795f4e33aa2011ffa422b6b8d5c641569d7ca2
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-10 15:01:18 +0300

    dependency injection

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -16,8 +16,7 @@ export class CoursesComponent {
   title = "List of Courses"
   courses: string[]
 
-  constructor(){
-      let service = new CoursesService()
+  constructor(service: CoursesService){
       this.courses = service.getCourses()
   }
 }
