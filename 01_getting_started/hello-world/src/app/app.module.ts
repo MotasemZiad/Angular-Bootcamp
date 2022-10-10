18c19f98bb1f74083578204d7281d45ca74ffb80
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-10 15:25:21 +0300

    registering the authors service in the providers

--- 01_getting_started/hello-world/src/app/app.module.ts
+++ 01_getting_started/hello-world/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AuthorsComponent } from './authors/authors.component';
     BrowserModule
   ],
   providers: [
-    CoursesService
+    CoursesService,
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
