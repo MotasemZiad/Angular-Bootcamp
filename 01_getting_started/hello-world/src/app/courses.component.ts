2254fa42c3c774961f2de21e3dd70ea9c460f0df
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-11 14:31:27 +0300

    template variables

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -3,23 +3,12 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <div (click)="onDivClick()">
-      <button class="btn btn-primary" [class.active]="isActive" (click)="onClick($event)"> Click Me!</button>
-    </div>
-    <br><br>
-    <button style="color: white;" [style.backgroundColor]="isActive ? 'blue' : 'red'"> Press Me!</button>
+    <input #email (keyup.enter)="onKeyUp(email.value)" />
   `,
 })
 export class CoursesComponent {
-    isActive = false;
 
-    onClick($event:any){
-      $event.stopPropagation(); // To disable event bubbling..
-      console.log("You click me!", $event);
-    }
-
-    onDivClick(){
-      console.log("You click the div");
-
-    }
+  onKeyUp(email: any){
+      console.log(email);
+  }
 }
