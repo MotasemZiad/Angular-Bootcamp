b8f1f60b5cbc6607c3bfb35bcd44116d53ceed3c
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-11 13:57:52 +0300

    event binding

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -3,9 +3,23 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <button class="btn btn-primary"> Click Me!</button>
+    <div (click)="onDivClick()">
+      <button class="btn btn-primary" [class.active]="isActive" (click)="onClick($event)"> Click Me!</button>
+    </div>
+    <br><br>
+    <button style="color: white;" [style.backgroundColor]="isActive ? 'blue' : 'red'"> Press Me!</button>
   `,
 })
 export class CoursesComponent {
+    isActive = false;
 
+    onClick($event:any){
+      $event.stopPropagation(); // To disable event bubbling..
+      console.log("You click me!", $event);
+    }
+
+    onDivClick(){
+      console.log("You click the div");
+
+    }
 }
