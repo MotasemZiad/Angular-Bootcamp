441a764ddbc4819dbc54775972190f87d8af2021
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-11 17:44:49 +0300

    pipes

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -4,11 +4,25 @@ import {Component} from '@angular/core';
   selector: 'courses',
   template: `
     <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    <br><br>
+    {{ "Title: " + (course.title | uppercase | lowercase) }} <br/>
+    {{ "Students: " + (course.students | number) }} <br/>
+    {{ "Rating: " + (course.rating | number: '1.1-2') }} <br/>
+    {{ "Price: " + (course.price | currency: 'GBP') }} <br/>
+    {{ "Release Date: " + (course.releaseDate | date: 'shortDate')  }} <br/>
   `,
 })
 export class CoursesComponent {
   email = "me@example.com";
   onKeyUp(){
       console.log(this.email);
+  }
+
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9752,
+    students: 90232,
+    price: 180.99,
+    releaseDate: new Date(2022, 7, 18)
   }
 }
