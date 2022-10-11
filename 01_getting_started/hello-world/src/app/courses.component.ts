3bbf3f5e0b3c3c0c123a2798061086998d6c16cf
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-11 12:33:51 +0300

    Adding Bootstrap

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -1,30 +1,11 @@
 import {Component} from '@angular/core';
-import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
-    <img [src]="imageUrl" />
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan">Something really useful</td>
-      </tr>
-    </table>
+    <button class="btn btn-primary"> Click Me!</button>
   `,
 })
 export class CoursesComponent {
-  title = "List of Courses"
-  courses: string[]
-  imageUrl = "https://www.copahost.com/blog/wp-content/uploads/2019/07/imgsize2.png"
-  colSpan = 2;
 
-  constructor(service: CoursesService){
-      this.courses = service.getCourses()
-  }
 }
