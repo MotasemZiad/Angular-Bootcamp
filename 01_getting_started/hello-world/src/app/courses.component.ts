025899fb5ad69edf6d55f83a048c2f37190800c6
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-11 11:59:19 +0300

    property binding

--- 01_getting_started/hello-world/src/app/courses.component.ts
+++ 01_getting_started/hello-world/src/app/courses.component.ts
@@ -10,11 +10,13 @@ import { CoursesService } from './courses.service';
         {{ course }}
       </li>
     </ul>
+    <img [src]="imageUrl" />
   `,
 })
 export class CoursesComponent {
   title = "List of Courses"
   courses: string[]
+  imageUrl = "https://www.copahost.com/blog/wp-content/uploads/2019/07/imgsize2.png"
 
   constructor(service: CoursesService){
       this.courses = service.getCourses()
