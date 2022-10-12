4595b90da3974df812ae8593aae6beca74bb7c8f
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-12 16:53:01 +0300

    refactoring...

--- 01_getting_started/hello-world/src/app/title.pipe.ts
+++ 01_getting_started/hello-world/src/app/title.pipe.ts
@@ -8,22 +8,29 @@ export class TitlePipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): unknown {
     if(!value) return null
 
-    let prepositions = [
-     "of",
-     "the"
-    ]
-
     let words = value.split(' ')
 
     for(let i = 0; i < words.length; i++){
-      if(i > 0 && prepositions.includes(words[i].toLowerCase())){
-        words[i] = words[i].toLowerCase()
-      }else {
-        words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase()
-      }
+      let word = words[i]
+      if(i > 0 && this.isPreposition(word))
+        word = word.toLowerCase()
+      else
+        word = this.capitalize(word)
     }
 
     return words.join(' ')
+  }
+
+  private isPreposition(word: string): boolean {
+    let prepositions = [
+      "of",
+      "the"
+     ]
+     return prepositions.includes(word.toLowerCase());
+  }
+
+  private capitalize(word: string): string {
+    return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
   }
 
 }
