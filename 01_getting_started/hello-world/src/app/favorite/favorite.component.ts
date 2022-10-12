5610dd57880b56e1f3f03aa7ec72f74bb103954c
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-12 15:02:55 +0300

    exercise 1

---
+++ 01_getting_started/hello-world/src/app/favorite/favorite.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  isFavorite = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleFavorite(){
+    this.isFavorite = !this.isFavorite
+  }
+
+}
