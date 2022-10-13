7df480a7cec1756e2b0265ee846a5f62953fe90c
Motasem Abu Nema <moatasem.abunema@gmail.com>
2022-10-13 13:51:21 +0300

    alias name in inputs

--- 01_getting_started/hello-world/src/app/favorite/favorite.component.ts
+++ 01_getting_started/hello-world/src/app/favorite/favorite.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent implements OnInit {
-  @Input() isFavorite = false;
+  @Input('isFavorite') isFavorite = false;
   constructor() { }
 
   ngOnInit(): void {
