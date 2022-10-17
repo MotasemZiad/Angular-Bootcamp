bfeface914667037438b2fc43f3b7c0fc71c02e1
MotasemZiad <miziad2000@gmail.com>
2022-10-17 10:56:04 +0300

    Angular Directives `initial commit`

--- 01_getting_started/hello-world/src/app/app.component.ts
+++ 01_getting_started/hello-world/src/app/app.component.ts
@@ -7,9 +7,5 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tweet = {
-    body: 'Here is the body of the tweet...',
-    isLiked: false,
-    likesCount: 0
-  }
+    courses = [1, 2]
 }
