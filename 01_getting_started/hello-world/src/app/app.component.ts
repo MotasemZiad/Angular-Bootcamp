121d4bbf4590433f5f0b26366e39486ee308f410
MotasemZiad <miziad2000@gmail.com>
2022-10-19 15:51:40 +0300

    change detection

--- 01_getting_started/hello-world/src/app/app.component.ts
+++ 01_getting_started/hello-world/src/app/app.component.ts
@@ -6,13 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    viewMode = 'map'
-
     courses = [
       {'id': 1, 'name': 'Programming'},
       {'id': 2, 'name': 'Cloud Computing'},
       {'id': 3, 'name': 'Networking'},
-      {'id': 4, 'name': 'Linux'},
+      {'id': 4, 'name': 'Operating Systems'},
       {'id': 5, 'name': 'Database Administration'},
     ]
+
+    onAddSomething(course: Course){
+      this.courses.push(course)
+    }
+
+    onAdd(){
+      this.courses.push({'id': 6, 'name': 'Distributed Systems'})
+    }
+
+    onRemove(course: Course){
+      let index = this.courses.indexOf(course)
+      this.courses.splice(index, 1)
+    }
+
+    onChange(course: Course){
+      course.name = 'UPDATED'
+    }
+}
+
+interface Course {
+  id: number
+  name: string
 }
