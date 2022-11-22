ff2b8e0f4b2bca210fb85df995b3e13c24a7f90e
MotasemZiad <miziad2000@gmail.com>
2022-11-22 18:36:30 +0200

    Specific validation errors

--- 01_getting_started/hello-world/src/app/signup-form/signup-form.component.ts
+++ 01_getting_started/hello-world/src/app/signup-form/signup-form.component.ts
@@ -7,8 +7,14 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]),
   })
 
 
