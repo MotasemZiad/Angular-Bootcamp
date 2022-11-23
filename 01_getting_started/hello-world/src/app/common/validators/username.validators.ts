c62aa1506a77ab7e512915ba7043deea724331b5
MotasemZiad <miziad2000@gmail.com>
2022-11-23 12:17:57 +0200

    Asynchronous Operations

--- 01_getting_started/hello-world/src/app/common/validators/username.validators.ts
+++ 01_getting_started/hello-world/src/app/common/validators/username.validators.ts
@@ -9,4 +9,21 @@ export class UsernameValidators {
 
     return null;
   }
+
+  static shouldBeUnique(control:AbstractControl<any, any>) : ValidationErrors | null {
+    setInterval(() => {
+      console.log("DONE");
+    }, 3000)
+
+    setTimeout(() => {
+      console.log("Ok");
+    }, 2000)
+
+    if (control.value === 'mosh')
+      return {
+        shouldBeUnique: true
+      }
+
+    return null
+  }
 }
