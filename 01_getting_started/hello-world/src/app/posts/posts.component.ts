9aa0b4bb14899d858908edd6ef850da31191c9d6
MotasemZiad <miziad2000@gmail.com>
2022-11-24 19:04:21 +0200

    refactoring...

--- 01_getting_started/hello-world/src/app/posts/posts.component.ts
+++ 01_getting_started/hello-world/src/app/posts/posts.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { identifierName } from '@angular/compiler';
 
 @Component({
   selector: 'posts',
